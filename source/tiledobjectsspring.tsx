ef8a5efc8d1aa41022dbb571c12c29dbf2b302e7
Alejandro <jairocastrillon@misena.edu.co>
2018-05-18 12:50:43 -0500

    Add tiled springfield files

---
+++ source/tiledobjectsspring.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="springfield" tilewidth="32" tileheight="32" tilecount="1300" columns="52">
+ <image source="tiledspringfield.png" trans="ff00ff" width="1672" height="805"/>
+</tileset>
